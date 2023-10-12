4035294ce3ef28f7e6b472902acd63a7e66473a0
Hasnat Rana <hxncodes@gmail.com>
2023-10-12 09:45:15 +0500

    Project initialized with all dependencies and Appwrite initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="h-screen bg-slate-800">
+      <h1 className="text-2xl text-slate-100">Welcome to Mega Blog Project</h1>
+    </div>
+  );
+}
+
+export default App;
