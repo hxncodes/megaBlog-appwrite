f88a2a8523a45625c164434a99e2a2a45a9fe700
Hasnat Rana <hxncodes@gmail.com>
2023-10-18 18:27:27 +0500

    More functional Components (Select, PostCard)

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,20 @@ import Header from "./Header/Header";
 import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
 import Footer from "./Footer/Footer";
 
-export { Container, Logo, Header, Button, LogoutBtn, Input, Footer };
+export {
+  Container,
+  Logo,
+  Header,
+  Button,
+  LogoutBtn,
+  Input,
+  Select,
+  PostCard,
+  Login,
+  Footer,
+};
